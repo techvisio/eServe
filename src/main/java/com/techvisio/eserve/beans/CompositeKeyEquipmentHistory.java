d908baa1c17adbae018e6f61ed3a95fe3554f2cc
pgusain <gusain.pradeep1986@gmail.com>
2016-12-07 22:27:48 +0530

    composite key issue

--- src/main/java/com/techvisio/eserve/beans/CompositeKeyEquipmentHistory.java
+++ src/main/java/com/techvisio/eserve/beans/CompositeKeyEquipmentHistory.java
@@ -32,5 +32,41 @@ public class CompositeKeyEquipmentHistory implements Serializable{
 	public void setVersion(Double version) {
 		this.version = version;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((equipmentDtlId == null) ? 0 : equipmentDtlId.hashCode());
+		result = prime * result + ((unitId == null) ? 0 : unitId.hashCode());
+		result = prime * result + ((version == null) ? 0 : version.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CompositeKeyEquipmentHistory other = (CompositeKeyEquipmentHistory) obj;
+		if (equipmentDtlId == null) {
+			if (other.equipmentDtlId != null)
+				return false;
+		} else if (!equipmentDtlId.equals(other.equipmentDtlId))
+			return false;
+		if (unitId == null) {
+			if (other.unitId != null)
+				return false;
+		} else if (!unitId.equals(other.unitId))
+			return false;
+		if (version == null) {
+			if (other.version != null)
+				return false;
+		} else if (!version.equals(other.version))
+			return false;
+		return true;
+	}
+	
 	
 }
